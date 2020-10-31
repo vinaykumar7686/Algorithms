29d46fe55735d23a370c683fdcd4045658f7643f
Vinay Kumar <49321180+vinaykumar7686@users.noreply.github.com>
2020-10-31 18:32:28 +0530

    Update Reverse String.py

--- Data Structures/Reverse String.py
+++ Data Structures/Reverse String.py
@@ -24,3 +24,37 @@ class Solution:
                 word = ""
                 
         return ans[1:]
+    
+# So called inplace reversal in string :()
+class Solution1:
+    def reverseWords(self, s: str) -> str:
+        '''
+        Some security measures are ignored as the question says the string is clean
+        '''
+        
+        def rev_word(l, h):
+            
+            while l<h:
+                s[l], s[h] = s[h], s[l]
+                l+=1
+                h-=1
+        
+        s = list(s)
+        
+        i = 0
+        n = len(s)
+        
+        l = 0
+        while i<n:
+            
+            if s[i] == " ":
+                rev_word(l,i-1)
+                l = i+1
+                i+=1
+                
+            i+=1
+        
+        else:
+            rev_word(l, n-1)
+            
+        return "".join(s)                
